4b6de37fe2988eea893730f456ce57501b69b50f
R26 <95674004+R26AST@users.noreply.github.com>
2024-05-19 11:56:05 +0700

    Add files via upload

---
+++ deadline.js
@@ -0,0 +1,6 @@
+var snbt_current_time, snbt_deadline_time, snbt_status, dataurl;
+
+snbt_current_time='Fri May 03 2024 02:42:16 GMT+0000';
+snbt_deadline_time='13 Jun 2024 15:00:00 +0700';
+deadline_status='sudah';
+dataurl='https://storage.googleapis.com/pengumuman-snbt-2023-prod-uj5f8';
